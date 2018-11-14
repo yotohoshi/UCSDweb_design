bb8bb1f8c427e64c14b03555198cac4b10125f1d
Yunshu (David) Gao <yug129@ucsd.edu>
2018-11-13 23:57:44 -0800

    Event top5 and filter algorithm completed

--- Django_UCSD/Event/models.py
+++ Django_UCSD/Event/models.py
@@ -2,15 +2,20 @@ from django.db import models
 import Company.models
 import django.core.validators
 import uuid
+from datetime import date, datetime, timedelta
 
 
 # Create your models here.
+# Global Variables
+TODAY = date.today()
+NOW = datetime.now()
 
 
 class Event(models.Model):
     event_id = models.UUIDField(primary_key=True, unique=True, default=uuid.uuid4, editable=False)
     event_name = models.CharField(max_length=150)
-    time = models.DateTimeField(auto_now=False, auto_created=False, auto_now_add=False)
+    date = models.DateField(auto_now=False, auto_created=False, auto_now_add=False)
+    time = models.TimeField(auto_now=False, auto_created=False, auto_now_add=False)
     company = models.ForeignKey(Company.models.Company, on_delete=models.PROTECT)
     location = models.CharField(max_length=100)
     type = models.IntegerField(
@@ -18,6 +23,7 @@ class Event(models.Model):
     description = models.CharField(max_length=1000, default="This event have no descriptions yet.")
     num_views = models.IntegerField(default=0, validators=[django.core.validators.MinValueValidator(0)])
     num_favorites = models.IntegerField(default=0, validators=[django.core.validators.MinValueValidator(0)])
+
     # category = models.IntegerField(max_length=2, null=False),
     # Getters
     @staticmethod
@@ -27,10 +33,43 @@ class Event(models.Model):
         else:
             return list(Event.objects.filter(company=company))
 
-
     # TODO Filter Functions
+    @staticmethod
+    def all_filter_by_today():
+        toReturn = list(Event.objects.filter(date=TODAY))
+        return toReturn
+
+    @staticmethod
+    def all_filter_by_month():
+        all_events = list(Event.objects.all())
+        toReturn = list()
+        for i in all_events:
+            if i.date.year == TODAY.year and i.date.month == TODAY.month:
+                toReturn.append(i)
+        return toReturn
+
+    @staticmethod
+    def all_filter_by_week():
+        start_week = TODAY - timedelta(TODAY.weekday())
+        end_week = start_week + timedelta(7)
+        toReturn = list(Event.objects.filter(date__range=[start_week, end_week]))
+        return toReturn
+
+    @staticmethod
+    def get_top_five():
+        all_events = list(Event.objects.all())
+        all_events.sort(key=Event.rank, reverse=True)
+        toReturn = list()
+        count = 0
+        while count < 5:
+            toReturn.append(all_events[count])
+            count += 1
+        return toReturn
 
     # Other functions
+    def rank(self):
+        return int(self.num_favorites * 10 + self.num_views)
+
     def update_num_favorite(self):
         self.num_favorites = self.user_set.all().count()
         return
@@ -49,11 +88,11 @@ class Event(models.Model):
             self.event_name = name
             return True
 
-    def set_time(self, time):
-        if type(time) != models.DateTimeField:
+    def set_date(self, Edate):
+        if type(Edate) != date:
             return False
         else:
-            self.time = time
+            self.date = Edate
 
     def set_company(self, company):
         if type(company) != Company.models.Company:
