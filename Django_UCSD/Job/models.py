095ce6c17e5a5190e92023a4261571af9190d24c
Yunshu (David) Gao <yug129@ucsd.edu>
2018-11-14 15:35:33 -0800

    fixed typos

--- Django_UCSD/Job/models.py
+++ Django_UCSD/Job/models.py
@@ -28,167 +28,167 @@ class Job(models.Model):
     Degree_Require = models.ManyToManyField(User.models.Degree, symmetrical=False, blank=True)
 
 
-# getter
+    # getter
 
-# get_Job_Company
-
-
-def get_Job_Company(company):
-    if type(company) != Company.models.Company:
-        return False
-    else:
-        return Job.objects.all.filter(Company=company)
-
-# get_Job_Work_Auth
+    # get_Job_Company
 
 
-def get_Job_Work_Auth(work_auth):
-    if type(work_auth) != WorkAuthorization:
-        return False
-    else:
-        return Job.objects.all.filter(WorkAuthorization=work_auth)
+    def get_Job_Company(company):
+        if type(company) != Company.models.Company:
+            return False
+        else:
+            return Job.objects.all.filter(Company=company)
 
-# get_Job_Position
-
-
-def get_Job_Position(position):
-    if type(position) != str:
-        return False
-    else:
-        return Job.objects.all.filter(job_position=position)
+    # get_Job_Work_Auth
 
 
-# get_Job_URL
+    def get_Job_Work_Auth(work_auth):
+        if type(work_auth) != WorkAuthorization:
+            return False
+        else:
+            return Job.objects.all.filter(WorkAuthorization=work_auth)
+
+    # get_Job_Position
 
 
-def get_Job_URL(url):
-    if type(url) != str:
-        return False
-    else:
-        return Job.objects.all.filter(job_URL=url)
-
-# get_Job_Major_Require
+    def get_Job_Position(position):
+        if type(position) != str:
+            return False
+        else:
+            return Job.objects.all.filter(job_position=position)
 
 
-def get_Job_Major_Require(major):
-    if type(major) != User.models.Major:
-        return False
-    else:
-        return Job.objects.all.filter(Major_Require=major)
+    # get_Job_URL
 
 
-# get_Job_Degree_Require
+    def get_Job_URL(url):
+        if type(url) != str:
+            return False
+        else:
+            return Job.objects.all.filter(job_URL=url)
+
+    # get_Job_Major_Require
 
 
-def get_Job_Degree_Require(degree):
-    if type(degree) != User.models.Degree:
-        return False
-    else:
-        return Job.objects.all.filter(Degree_Require=degree)
+    def get_Job_Major_Require(major):
+        if type(major) != User.models.Major:
+            return False
+        else:
+            return Job.objects.all.filter(Major_Require=major)
 
 
-# get_Job_Duration
+    # get_Job_Degree_Require
 
 
-def get_Job_Duration(duration):
-    if type(duration) != str:
-        return False
-    else:
-        return Job.object.all.filter(job_duration=duration)
-
-# get_Job_location
+    def get_Job_Degree_Require(degree):
+        if type(degree) != User.models.Degree:
+            return False
+        else:
+            return Job.objects.all.filter(Degree_Require=degree)
 
 
-def get_Job_location(location):
-    if type(location) != str:
-        return False
-    else:
-        return Job.object.all.filter(job_location=location)
+    # get_Job_Duration
 
 
-# get_Job_paid
+    def get_Job_Duration(duration):
+        if type(duration) != str:
+            return False
+        else:
+            return Job.object.all.filter(job_duration=duration)
+
+    # get_Job_location
 
 
-def get_Job_paid(paid):
-    if type(paid) != bool:
-        return False
-    else:
-        return Job.object.all.filter(job_paid=paid)
+    def get_Job_location(location):
+        if type(location) != str:
+            return False
+        else:
+            return Job.object.all.filter(job_location=location)
 
 
-# setter
-
-# set_Job_Position
+    # get_Job_paid
 
 
-def set_Job_Position(self, position):
-    if type(position) != str:
-        return False
-    else:
-        self.job_position = position
-        return True
-
-# set_Job_Company
+    def get_Job_paid(paid):
+        if type(paid) != bool:
+            return False
+        else:
+            return Job.object.all.filter(job_paid=paid)
 
 
-def set_Job_Company(self, company):
-    if type(company) != Company.models.Company:
-        return False
-    else:
-        self.company = company
-        return True
+    # setter
+
+    # set_Job_Position
 
 
-# set_Job_Duration
+    def set_Job_Position(self, position):
+        if type(position) != str:
+            return False
+        else:
+            self.job_position = position
+            return True
+
+    # set_Job_Company
 
 
-def set_Job_Duration(self, duration):
-    if type(duration) != str:
-        return False
-    else:
-        self.job_duration = duration
-        return True
+    def set_Job_Company(self, company):
+        if type(company) != Company.models.Company:
+            return False
+        else:
+            self.company = company
+            return True
 
 
-# set_Job_Major_Require
+    # set_Job_Duration
 
 
-def set_major(self, major):
-    if type(major) != User.models.Major:
-        return False
-    else:
-        self.Major_Require = major
-        return True
+    def set_Job_Duration(self, duration):
+        if type(duration) != str:
+            return False
+        else:
+            self.job_duration = duration
+            return True
 
 
-# set_Job_Degree_Require
+    # set_Job_Major_Require
 
 
-def set_degree(self, degree):
-    if type(degree) != User.models.Degree:
-        return False
-    else:
-        self.Degree_Require = degree
-        return True
+    def set_major(self, major):
+        if type(major) != User.models.Major:
+            return False
+        else:
+            self.Major_Require = major
+            return True
 
 
-# set_Job_Work_Auth
+    # set_Job_Degree_Require
 
 
-def set_Job_Work_Auth(self, work_auth):
-    if type(work_auth) != WorkAuthorization:
-        return False
-    else:
-        self.job_Work_Auth = work_auth
-        return True
+    def set_degree(self, degree):
+        if type(degree) != User.models.Degree:
+            return False
+        else:
+            self.Degree_Require = degree
+            return True
 
 
-# set_Job_paid
+    # set_Job_Work_Auth
 
 
-def set_Job_paid(self, paid):
-    if type(paid) != bool:
-        return False
-    else:
-        self.job_paid = paid
-        return True
+    def set_Job_Work_Auth(self, work_auth):
+        if type(work_auth) != WorkAuthorization:
+            return False
+        else:
+            self.job_Work_Auth = work_auth
+            return True
+
+
+    # set_Job_paid
+
+
+    def set_Job_paid(self, paid):
+        if type(paid) != bool:
+            return False
+        else:
+            self.job_paid = paid
+            return True
