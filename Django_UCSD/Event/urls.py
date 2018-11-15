8a815517f0ac5abdb34c709f569ad23e32328ad8
Yunshu (David) Gao <yug129@ucsd.edu>
2018-11-14 20:32:04 -0800

    Event visulization completed
    Add to favorite not done due to log in /user management unimplemented

--- Django_UCSD/Event/urls.py
+++ Django_UCSD/Event/urls.py
@@ -1,9 +1,15 @@
-
+from django.conf.urls import url
 from django.urls import include, path
 
 from Event import views
 
+# app_name = 'Event'
 urlpatterns = [
 
-    path('Event/templates/', views.event.as_view(), name='event'),
+    url(r'^event$', views.event.as_view(), name='event'),
+    url(r'^event_list$', views.EventListView.as_view(), name='event_list'),
+    # url(r'^event_list_5$', views.EventListViewTop5.as_view(), name='event_list_top5'),
+    url(r'^event_list&filter=month$', views.EventListViewMonth.as_view(), name='event_list_month'),
+    url(r'^event_list&filter=week$', views.EventListViewWeek.as_view(), name='event_list_week'),
+    url(r'^event_list&filter=today$', views.EventListViewToday.as_view(), name='event_list_today'),
 ]
--- Django_UCSD/Event/views.py
+++ Django_UCSD/Event/views.py
@@ -10,7 +10,63 @@ class event(TemplateView):
 
 
 class EventListView(ListView):
+    context_object_name = 'events'
     model = Event
+    template_name = 'event_list.html'
 
     def get_queryset(self):
         return Event.get_all()
+
+    def top5(self):
+        return Event.get_top_five()
+
+    def by_month(self, request):
+        return Event.all_filter_by_month()
+
+    def by_week(self):
+        return Event.all_filter_by_week()
+
+    def by_today(self):
+        return Event.all_filter_by_today()
+
+
+class EventListViewMonth(ListView):
+    context_object_name = 'events'
+    model = Event
+    template_name = 'event_list.html'
+
+    def get_queryset(self):
+        return Event.all_filter_by_month()
+
+    def top5(self):
+        return Event.get_top_five()
+
+
+class EventListViewWeek(ListView):
+    context_object_name = 'events'
+    model = Event
+    template_name = 'event_list.html'
+
+    def get_queryset(self):
+        return Event.all_filter_by_week()
+
+    def top5(self):
+        return Event.get_top_five()
+
+
+class EventListViewToday(ListView):
+    context_object_name = 'events'
+    model = Event
+    template_name = 'event_list.html'
+
+    def get_queryset(self):
+        return Event.all_filter_by_today()
+
+    def top5(self):
+        return Event.get_top_five()
+
+
+class EventDetailView(DetailView):
+    context_object_name = 'event_detail'
+    model = Event
+    # template_name = "event_list.html"
