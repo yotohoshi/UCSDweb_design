ac1efa7d91d1e3f083b203e9dc15a4385a941c68
Shaoyuan Xu <xushaoyuan3@gmail.com>
2018-11-15 20:54:40 -0800

    new version of front end. changed event ad search

---
+++ front-end/V1.1/events2.js
@@ -0,0 +1,16 @@
+
+$( document ).ready(function() {
+     $('.leftmenutrigger').on('click', function(e) {
+     $('.side-nav').toggleClass("open");
+     e.preventDefault();
+    });
+});
+
+function toggleIcon(e) {
+    $(e.target)
+        .prev('.panel-heading')
+        .find(".more-less")
+        .toggleClass('glyphicon-plus glyphicon-minus');
+}
+$('.panel-group').on('hidden.bs.collapse', toggleIcon);
+$('.panel-group').on('shown.bs.collapse', toggleIcon);
