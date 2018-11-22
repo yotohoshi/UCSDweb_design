9d8566dc56818e31ae085960c07a7ce98b7c7fda
luyu <luy015@ucsd.edu>
2018-11-22 00:45:20 -0800

    job modified

--- front-end/V1.2.5 edition1/index.js
+++ front-end/V1.2.5 edition1/index.js
@@ -3,11 +3,11 @@ $(document).ready(function () {
     //Load Slides with Capion
     $('#Topright').load('index_top_right.html');
     //Load Slides with Capion
-    $('#advertisement').load('banner.html');
+    <!--$('#advertisement').load('banner.html');-->
     // Load SignUp PopUp
     $('#signup-popup').load('signup.html');
     // Load search bar
-    $('#searchbar').load('searchbar.html');
+    <!--$('#searchbar').load('searchbar.html');-->
     // Load Footnote Circles
     $('#footnote').load('footnote_circles.html');
     // alert('success!!!');
